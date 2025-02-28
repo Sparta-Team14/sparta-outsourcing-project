04ed6edb9b53edecb2c55397d2269a025101c57e
chungwonJ <wjdcjddnjs1234@gmail.com>
2025-02-28 21:57:39 +0900

    feat : foodstore setting refactor : BaseEntity camel

--- src/main/java/com/example/jeogiyoproject/domain/foodstore/entity/FoodStore.java
+++ src/main/java/com/example/jeogiyoproject/domain/foodstore/entity/FoodStore.java
@@ -1,29 +1,60 @@
 package com.example.jeogiyoproject.domain.foodstore.entity;
 
+import com.example.jeogiyoproject.domain.account.entity.Account;
 import com.example.jeogiyoproject.domain.base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Getter
+@Setter
 @Entity
-@Table(name = "foodstores")
 @NoArgsConstructor
+@Table(name = "foodstores")
 public class FoodStore extends BaseEntity {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // 가게번호
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column(length = 100, nullable = false)
-    private String title; // 가게명
-    @Column(columnDefinition = "TEXT", nullable = false)
-    private String address; // 주소
-    @Column
-    private Integer min_price; // 최소주문금액
-    @Column
-    private LocalDateTime open_at; // 오픈시간
-    @Column
-    private LocalDateTime close_at; // 마감시간
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Account account;
 
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private Integer minPrice;
+
+    @Column(nullable = false)
+    private LocalDateTime openAt;
+
+    @Column(nullable = false)
+    private LocalDateTime closeAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    public FoodStore(
+            Account account,
+            String title,
+            String address,
+            Integer minPrice,
+            LocalDateTime openAt,
+            LocalDateTime closeAt
+    ) {
+        this.account = account;
+        this.title = title;
+        this.address = address;
+        this.minPrice = minPrice;
+        this.openAt = openAt;
+        this.closeAt = closeAt;
+    }
 }
