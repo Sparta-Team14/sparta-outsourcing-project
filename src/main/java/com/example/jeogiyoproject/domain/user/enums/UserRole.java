e043db192a6c65b7c7f00705e6fce95ed8554d98
dawon <aodlstory321@gmail.com>
2025-03-04 09:54:44 +0900

    feat: JWT 필터, 유틸 추가

---
+++ src/main/java/com/example/jeogiyoproject/domain/user/enums/UserRole.java
@@ -0,0 +1,17 @@
+package com.example.jeogiyoproject.domain.user.enums;
+
+import com.example.jeogiyoproject.global.exception.CustomException;
+import com.example.jeogiyoproject.global.exception.ErrorCode;
+
+import java.util.Arrays;
+
+public enum UserRole {
+    OWNER, USER;
+
+    public static UserRole of(String role) {
+        return Arrays.stream(UserRole.values())
+                .filter(userRole -> userRole.name().equalsIgnoreCase(role))
+                .findFirst()
+                .orElseThrow(() -> new CustomException(ErrorCode.ROLE_IS_WRONG));
+    }
+}
---
+++ src/main/java/com/example/jeogiyoproject/global/JwtFilter.java
@@ -0,0 +1,80 @@
+package com.example.jeogiyoproject.global;
+
+import com.example.jeogiyoproject.domain.user.enums.UserRole;
+import com.example.jeogiyoproject.global.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ClaimsMutator;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.nio.charset.MalformedInputException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        String url = request.getRequestURI();
+
+        if (url.startsWith("/auth")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String baererJwt = request.getHeader("Authorization");
+
+        if (baererJwt == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "JWT 토큰이 필요합니다!");
+            return;
+        }
+
+        String jwt = jwtUtil.substringToken(baererJwt);
+
+        try {
+            Claims claims = jwtUtil.extractClaims(jwt);
+            if (claims == null) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 토큰입니다");
+                return;
+            }
+
+            UserRole userRole = UserRole.valueOf(claims.get("userRole", String.class));
+
+            request.setAttribute("userId", Long.parseLong(claims.getSubject()));
+            request.setAttribute("email", claims.get("email"));
+            request.setAttribute("userRole", claims.get("userRole"));
+
+            if (url.startsWith("/admin")) {
+                if (!UserRole.OWNER.equals(userRole)) {
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN, "접근 권한이 없습니다.");
+                }
+                chain.doFilter(request, response);
+                return;
+            }
+            chain.doFilter(request, response);
+        } catch (ServletException | MalformedInputException e) {
+            log.error("유효하지 않는 JWT 서명입니다.", e);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지않는 JWT 토큰입니다.");
+        } catch (ExpiredJwtException e) {
+            log.error("만료된 JWT Token입니다", e);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.error("지원하지않는 JWT 토큰입니다");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "지원하지않는 JWT 토큰입니다.");
+        } catch (Exception e) {
+            log.error("유효하지않는 JWT 토큰입니다");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "유효하지 않는 JWT 토큰입니다.");
+        }
+
+    }
+}
