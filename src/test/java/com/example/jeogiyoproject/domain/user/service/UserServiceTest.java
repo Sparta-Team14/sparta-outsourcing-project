6999949ae28db74e8435e90e8e015d654bb60580
dawon <aodlstory321@gmail.com>
2025-03-05 10:11:24 +0900

    chore: JPQL 삭제

--- src/test/java/com/example/jeogiyoproject/domain/user/service/UserServiceTest.java
+++ src/test/java/com/example/jeogiyoproject/domain/user/service/UserServiceTest.java
@@ -1,4 +1,25 @@
 package com.example.jeogiyoproject.domain.user.service;
 
+import com.example.jeogiyoproject.domain.user.controller.UserController;
+import com.example.jeogiyoproject.global.AuthUserArgumentResolver;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+@Import(AuthUserArgumentResolver.class)
 public class UserServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private UserService userService;
 }
